2ab47d6e4f46b22959092c18d76c52b1bd6987ad
Yuri Volkov <volkov.yuri.alex@gmail.com>
2026-02-05 19:29:44 +0100

    Switch EPD off and deactivate pins to decrease current consumption

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// #![deny(unsafe_code)]
+#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
@@ -11,7 +11,10 @@ use embedded_graphics::prelude::*;
 use embedded_hal::digital::OutputPin;
 use embedded_hal::delay::DelayNs;
 use embedded_hal_bus::spi::ExclusiveDevice;
-use epd_waveshare::{epd1in02, prelude::*};
+use epd_waveshare::{
+    epd1in02::{Epd1in02, Display1in02},
+    prelude::*
+};
 use fixed::types::*;
 // use hal::{
 //     clocks::Clock,
@@ -23,11 +26,13 @@ use fixed::types::*;
 // };
 // use inverted_pin::InvertedPin;
 use hal::{
-    gpio::{gpioa, Output, PushPull},
-    i2c::Config,
+    gpio::{gpioa, gpioa::PA, Analog, Input, PullUp, PullDown, OpenDrain, Output, PushPull, PB7, PB6, PA5, PA7},
+    i2c::{Config, I2c},
+    pac::{SPI1, TIM16, TIM17, I2C1, TIM14},
     prelude::*,
     rcc,
-    spi::{self, Mode, Phase, Polarity},
+    spi::{self, Mode, Phase, Polarity, SpiBus, NoMiso},
+    timer::delay::Delay,
 };
 use inverted_pin::InvertedPin;
 use panic_probe as _;
@@ -36,6 +41,8 @@ use rtic_monotonics::systick::prelude::*;
 use sht4x::{Sht4x, Precision};
 use stm32g0xx_hal as hal;
 use u8g2_fonts::{fonts, types::*, FontRenderer};
+
+use g031_sht40::deactivate::{ActivatedEpdPins, DeactivatedEpdPins};
 
 
 systick_monotonic!(Mono, 1000);
@@ -47,9 +54,9 @@ mod app {
     use super::*;
 
 
-    type SpiDev = ExclusiveDevice<spi::SpiBus<hal::pac::SPI1, (gpioa::PA5<hal::gpio::Analog>, spi::NoMiso, gpioa::PA7<hal::gpio::Analog>)>, gpioa::PA<Output<PushPull>>, &'static mut hal::timer::delay::Delay<hal::pac::TIM17>>;
-    type Epd = epd1in02::Epd1in02<ExclusiveDevice<spi::SpiBus<hal::pac::SPI1, (gpioa::PA5<hal::gpio::Analog>, spi::NoMiso, gpioa::PA7<hal::gpio::Analog>)>, gpioa::PA<Output<PushPull>>, &'static mut hal::timer::delay::Delay<hal::pac::TIM17>>, gpioa::PA<hal::gpio::Input<hal::gpio::Floating>>, gpioa::PA<Output<PushPull>>, gpioa::PA<Output<PushPull>>, hal::timer::delay::Delay<hal::pac::TIM16>>;
-    type Sensor = sht4x::Sht4x<hal::i2c::I2c<hal::pac::I2C1, hal::gpio::PB7<Output<hal::gpio::OpenDrain>>, hal::gpio::PB6<Output<hal::gpio::OpenDrain>>>, hal::timer::delay::Delay<hal::pac::TIM14>>;
+    type SpiDev = ExclusiveDevice<SpiBus<SPI1, (PA5<Analog>, NoMiso, PA7<Analog>)>, PA<Output<PushPull>>, &'static mut Delay<TIM17>>;
+    type Epd = Epd1in02<SpiDev, PA<Input<PullDown>>, PA<Output<PushPull>>, PA<Output<PushPull>>, Delay<TIM16>>;
+    type Sensor = sht4x::Sht4x<I2c<I2C1, PB7<Output<OpenDrain>>, PB6<Output<OpenDrain>>>, Delay<TIM14>>;
 
 
     #[shared]
@@ -59,11 +66,13 @@ mod app {
     struct Local {
         epd: Epd,
         spi_dev: SpiDev,
-        epd_delay: hal::timer::delay::Delay<hal::pac::TIM16>,
+        epd_delay: Delay<TIM16>,
         // rtc: rtc::RealTimeClock,
         sensor: Sensor,
-        sensor_delay: hal::timer::delay::Delay<hal::pac::TIM14>,
-        en_sensor: InvertedPin<gpioa::PA<Output<PushPull>>>,
+        sensor_delay: Delay<TIM14>,
+        en_sensor: InvertedPin<gpioa::PA<Output<OpenDrain>>>,
+        en_epd: gpioa::PA<Output<PushPull>>,
+        rcc: rcc::Rcc,
     }
 
     #[init]
@@ -73,6 +82,8 @@ mod app {
         let mut rcc = cx.device.RCC.freeze(rcc::Config::hsi(rcc::Prescaler::Div16));
 
         Mono::start(cx.core.SYST, rcc.clocks.sys_clk.to_Hz());
+        // TODO: Fix: for unknown reason the first Mono activation takes above ten seconds
+        // At least let it happen when nothing is still started
         Mono.delay_ms(1);
 
         let gpioa = cx.device.GPIOA.split(&mut rcc);
@@ -80,10 +91,11 @@ mod app {
         // let led = gpioa.pa4.into_push_pull_output().downgrade();
 
         // Epd display
-        let epd_busy = gpioa.pa3.into_floating_input().downgrade();
+        let mut en_epd = gpioa.pa0.into_push_pull_output_in_state(PinState::High).downgrade();
+        let epd_busy = gpioa.pa3.into_pull_down_input().downgrade();
         let epd_reset = gpioa.pa2.into_push_pull_output().downgrade();
         let epd_dc = gpioa.pa8.into_push_pull_output().downgrade();
-        let epd_cs = gpioa.pa4.into_push_pull_output().downgrade();
+        let epd_cs = gpioa.pa6.into_push_pull_output().downgrade();
         let spi_clk = gpioa.pa5;
         let spi_mosi = gpioa.pa7;
 
@@ -109,8 +121,9 @@ mod app {
         )
         .expect("SpiDevice error");
 
+        en_epd.set_high().ok();
         let mut epd_delay = cx.device.TIM16.delay(&mut rcc);
-        let mut epd = epd1in02::Epd1in02::new(
+        let mut epd = Epd1in02::new(
             &mut spi_dev,
             epd_busy,
             epd_dc,
@@ -122,10 +135,12 @@ mod app {
         epd.clear_frame(&mut spi_dev, &mut epd_delay).expect("Epd error");
         epd.display_frame(&mut spi_dev, &mut epd_delay).expect("Epd error");
         epd.sleep(&mut spi_dev, &mut epd_delay).expect("Epd error");
-
+        en_epd.set_low().ok();
+        
+        let _ = DeactivatedEpdPins::steal_epd_pins(&mut rcc);
 
         // Sht40 sensor
-        let en_sensor = gpioa.pa12.into_push_pull_output().downgrade();
+        let en_sensor = gpioa.pa12.into_open_drain_output_in_state(PinState::High).downgrade();
         let en_sensor = InvertedPin::new(en_sensor);
         let sda = gpiob.pb7.into_open_drain_output_in_state(PinState::High);
         let scl = gpiob.pb6.into_open_drain_output_in_state(PinState::High);
@@ -160,7 +175,8 @@ mod app {
         // // Let the core enter deep-sleep while waiting on wfi
         // let mut scb = cx.core.SCB;
         // scb.set_sleepdeep();
-       defmt::info!("Initialization finished!");
+        
+        defmt::info!("Initialization finished!");
 
         main_task::spawn().ok();
         // heartbeat::spawn().ok();
@@ -175,25 +191,28 @@ mod app {
                 sensor,
                 sensor_delay,
                 en_sensor,
+                en_epd,
+                rcc,
             },
         )
     }
 
 
-    #[task(local = [epd, spi_dev, epd_delay, sensor, sensor_delay, en_sensor], priority = 1)]
+    #[task(local = [epd, spi_dev, epd_delay, sensor, sensor_delay, en_sensor, en_epd, rcc], priority = 1)]
     async fn main_task(cx: main_task::Context) {
 
         let main_task::LocalResources
-            {epd, spi_dev, epd_delay, sensor, sensor_delay, en_sensor, ..} = cx.local;
+            {epd, spi_dev, epd_delay, sensor, sensor_delay, en_sensor, en_epd, rcc, ..} = cx.local;
 
-
-        let mut frame = epd1in02::Display1in02::default();
-        frame.set_rotation(DisplayRotation::Rotate0);
+        let mut frame = Display1in02::default();
+        frame.set_rotation(DisplayRotation::Rotate180);
         
         let font = FontRenderer::new::<fonts::u8g2_font_logisoso24_tf>();
 
         let mut prev_temperature = I16F16::ZERO;
         let mut prev_humidity = I16F16::ZERO;
+
+        let mut epd_pins = DeactivatedEpdPins::steal_epd_pins(rcc);
 
         loop {
             let mut buf = [0u8; 40];
@@ -246,11 +265,15 @@ mod app {
             .expect("Render error");
 
             // Display a message
+            let activated_epd_pins = epd_pins.reactivate();
+            en_epd.set_high().ok();
             epd.wake_up(spi_dev, epd_delay).expect("Epd error");
             epd
                 .update_and_display_frame(spi_dev, frame.buffer(), epd_delay)
                 .expect("Epd error");
             epd.sleep(spi_dev, epd_delay).expect("Epd error");
+            en_epd.set_low().ok();
+            epd_pins = activated_epd_pins.deactivate();
 
             Mono::delay(10.secs()).await;
             // hw_config::sleep();
@@ -287,7 +310,7 @@ mod app {
     fn idle(_: idle::Context) -> ! {
 
         loop {
-            continue;
+            cortex_m::asm::wfi();
         }
     }
 }
